946709332335ded4df425e7be365b9e00a690312
Yifan Wu <patwonder@163.com>
2015-07-19 17:32:45 +0800

    Add package tools for Windows platform.

---
+++ tools/buildxpi.bat
@@ -0,0 +1,8 @@
+set XPI_NAME=autoproxy.xpi
+cd ..
+del /f/q %XPI_NAME%
+tools\7za a %XPI_NAME% chrome\
+tools\7za a %XPI_NAME% defaults\
+tools\7za a %XPI_NAME% components\
+tools\7za a %XPI_NAME% chrome.manifest
+tools\7za a %XPI_NAME% install.rdf
